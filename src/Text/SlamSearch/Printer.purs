36d90408c4084ba8d38f8fe685911bdeccd1dc59
Maxim Zimaliev <zimaliev@yandex.ru>
2015-04-07 18:15:05 +0300

    strValue escapes

--- src/Text/SlamSearch/Printer.purs
+++ src/Text/SlamSearch/Printer.purs
@@ -1,9 +1,10 @@
 module Text.SlamSearch.Printer where
 
 import Data.Foldable (fold, foldr)
-import Data.Array (reverse)
-import Data.String (trim)
+import Data.Array (reverse, (\\), length)
+import Data.String (trim, split)
 import Text.SlamSearch.Types
+import Text.SlamSearch.Parser.Tokens (keyChars)
 import Data.Semiring.Free
 
 strLabel :: Label -> String
@@ -13,9 +14,13 @@ strLabel l = case l of
 
 strValue :: Value -> String
 strValue v = case v of
-  Text str -> str
+  Text str -> quote str
   Range bot up -> bot <> ".." <> up
   Tag str -> "#" <> str
+  where quote str =
+          if length ((split "" str) \\ keyChars) > 0 then
+            "\"" <> str <> "\""
+          else str
 
 strPredicate :: Predicate -> String
 strPredicate pr = case pr of
